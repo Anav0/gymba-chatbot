9fee8f31269b3e76d1d2bd16894c56620e0bfcf1
Igor Motyka <igor_motyka@mail.com>
2020-02-03 19:25:00 +0100

    Different build file

--- app/main.py
+++ app/main.py
@@ -4,10 +4,11 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot.trainers import UbuntuCorpusTrainer
 from dotenv import load_dotenv
 import os
-
+import logging
 
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def get_bot_response():
@@ -16,22 +17,22 @@ def get_bot_response():
         if userText is None:
             return str("No message argument provided")
         return str(pan_japa.get_response(userText))
-    except Exception:
+    except Exception as e:
+        print(e)
         return str("Something went wrong")
 
-
-if __name__ == "__main__":
-    print("1")
-    load_dotenv(verbose=True)
-    print("2")
-    pan_japa = ChatBot("Pan Japa",
-                     storage_adapter = "chatterbot.storage.MongoDatabaseAdapter",
-                     database = "gymba",
-                     database_uri = os.getenv("MONGO_CONNECTION_STRING"))
-    print("3")
-    trainer = UbuntuCorpusTrainer(pan_japa)
-    print("4")
-    trainer.train()
-    print("5")
-    app.run(host='0.0.0.0', debug=True, port=80)
-    print("6")
+print("Loading enviroment variables...")
+load_dotenv(verbose=True)
+print("Connecting chatbot to Db...")
+logging.basicConfig(level=logging.DEBUG)
+pan_japa = ChatBot("Pan Japa",
+                    storage_adapter = "chatterbot.storage.MongoDatabaseAdapter",
+                    database = "gymba",
+                    database_uri = os.getenv("MONGO_CONNECTION_STRING"))
+print("Creating trainer...")
+trainer = UbuntuCorpusTrainer(pan_japa)
+print("Training chatbot...")
+trainer.train()
+print("Training is finished!")
+app.run(host='0.0.0.0', debug=True, port=80)
+print("Done!")
